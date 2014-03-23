e08eaebf2b2e876fad26992514712345ab258fb7
Andrew Noyes <anoyes34@gmail.com>
2014-03-23 14:27:36 -0400

    decode html entities
    thanks to
    http://stackoverflow.com/questions/3093342/unescaping-html-entities-nnnn-into-plain-utf-8

--- twitter-fortune.sh
+++ twitter-fortune.sh
@@ -16,4 +16,5 @@ curl -s http://greptweet.com/f/$TWITTER_HANDLE > /dev/null
 curl -s http://greptweet.com/u/$TWITTER_HANDLE/tweets.txt \
     | cut -f3 -d'|' \
     | grep -v '\<RT\>\|\<http\|@' \
+    | perl -MHTML::Entities -pe'binmode STDOUT, ":utf8"; HTML::Entities::decode_entities($_)' \
     | awk "1; { print \"\t\t-- @$TWITTER_HANDLE\n%\"; }"
