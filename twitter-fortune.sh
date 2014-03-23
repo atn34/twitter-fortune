1e070773cc106cc5a319cfb2da85401321d7df9f
Andrew Noyes <anoyes34@gmail.com>
2014-03-23 14:44:12 -0400

    perl was botching unicode. simple sed pipeline instead

--- twitter-fortune.sh
+++ twitter-fortune.sh
@@ -9,6 +9,10 @@ if [ -z $1 ] ; then
     exit
 fi
 
+decode_entities() {
+    sed 's/&amp;/\&/g' | sed 's/&lt;/</g' | sed 's/&gt;/>/g'
+}
+
 TWITTER_HANDLE=$1
 
 curl -s http://greptweet.com/f/$TWITTER_HANDLE > /dev/null
@@ -16,5 +20,5 @@ curl -s http://greptweet.com/f/$TWITTER_HANDLE > /dev/null
 curl -s http://greptweet.com/u/$TWITTER_HANDLE/tweets.txt \
     | cut -f3 -d'|' \
     | grep -v '\<RT\>\|\<http\|@' \
-    | perl -MHTML::Entities -pe'binmode STDOUT, ":utf8"; HTML::Entities::decode_entities($_)' \
+    | decode_entities \
     | awk "1; { print \"\t\t-- @$TWITTER_HANDLE\n%\"; }"
